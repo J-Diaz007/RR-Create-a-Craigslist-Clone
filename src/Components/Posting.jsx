9620bad6b93613f0ac5994de3ff8dd47d64ef8b5
JDiaz <jdiaz_007@outlook.com>
2023-09-23 22:05:19 -0700

    used props to add images and pricing in posting.jsx

--- src/Components/Posting.jsx
+++ src/Components/Posting.jsx
@@ -2,7 +2,9 @@ export default function Posting(props) {
     return (
         <div>
             <h2>{props.data.title}</h2>
+            <img src={props.data.imageURL} alt={props.data.title} />
             <p>{props.data.description}</p>
+            <p>{props.data.price}</p>
         </div>
     )
 }
